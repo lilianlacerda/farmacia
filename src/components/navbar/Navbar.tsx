eb069f9c9d690e9e2f411e10e8e54dd64251a673
lilianlacerda <lilianraiany@gmail.com>
2026-07-07 11:32:03 -0300

    criacao do componente navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div className="w-full flex justify-center py-4 bg-slate-700 text-white">
+        <div className="container flex justify-between text-lg mx-8">
+          <Link to="/home" className="text-2xl font-bold">
+            Farmacia
+          </Link>
+          <div className="flex gap-4">
+            Categorias Produtos Cadastrar Produtos
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
